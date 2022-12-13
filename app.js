fe9ef7e742abfca5fa465fbefad5f4cb7900f752
adriantheodor <aatheodor@gmail.com>
2022-12-13 15:56:55 -0500

    tues aft 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import mongoose from "mongoose"
 import express from 'express'
 import cors from 'cors'
-import session from 'express-session';
+import session from 'express-session'
+import mongoose from "mongoose"
 import UsersController from "./users/users-controller.js";
 import SessionController from "./session-controller.js";
 import {ReviewsController} from "./reviews/reviews-controller.js";
@@ -13,25 +13,25 @@ const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: 5000,
+    autoIndex: false,
     maxPoolSize: 10, // Maintain up to 10 socket connections
     socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     family: 4 // Use IPv4, skip trying IPv6
 }
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://0.0.0.0:27017/anidb"
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb+srv://adrian:RcJ8nOXvz7Qd8BZQ@cluster0.q3n6zem.mongodb.net/?retryWrites=true&w=majority"
 //console.log(CONNECTION_STRING)
+
 mongoose.connect(CONNECTION_STRING, options);
 
 const port = process.env.PORT || 4000
 
 const app = express();
 
-app.use(cors(
-    {
-        credentials: true,
-        origin: 'http://localhost:3000'
-    }
-))
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}))
 
 
 
