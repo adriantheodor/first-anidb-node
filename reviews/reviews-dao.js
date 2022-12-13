9c7e6eccbce0326d53d7a56f8e103ccb30b5d4f7
adriantheodor <aatheodor@gmail.com>
2022-12-13 16:06:16 -0500

    tues aft 3

--- reviews/reviews-dao.js
+++ reviews/reviews-dao.js
@@ -1,9 +1,13 @@
 import reviewsModel from "./reviews-model.js";
 
-export const deleteReview = (reviewToDelete) => reviewsModel.deleteOne({_id: reviewToDelete.rid})
+export const deleteReview = (reviewToDelete) =>
+    reviewsModel.deleteOne({_id: reviewToDelete.rid})
 
-export const createReview = (review) => reviewsModel.create(review)
+export const createReview = (review) =>
+    reviewsModel.create(review)
 
-export const findReviewsByAnime = (animeId) => reviewsModel.find({animeId}).populate('author').exec()
+export const findReviewsByAnime = (animeId) =>
+    reviewsModel.find({animeId}).populate('author').exec()
 
-export const findReviewsByAuthor = (author) => reviewsModel.find({author})
+export const findReviewsByAuthor = (author) =>
+    reviewsModel.find({author})
